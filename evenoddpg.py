b28a4a4a07fbc3ad5c6eba7300450962510f2a1a
Niranjan  sheelavant <niranjansheelavant.ns@gmail.com>
2025-10-06 16:32:25 +0530

    Create evenoddpg.py

---
+++ evenoddpg.py
@@ -0,0 +1,8 @@
+#write python program to check whether a number is odd or even
+a=int(input("enter a number:"))
+if a %2==0:
+    print("the number is even!")
+else:
+    print("the number is odd")  
+
+
